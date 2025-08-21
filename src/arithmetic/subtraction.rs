b8f31a9722ed778465a3fd946c9216b2e9fc536d
arctic-hen7 <arctic.hen@pm.me>
2025-08-21 10:57:16 +1000

    docs: added documentation for subtraction

--- src/arithmetic/subtraction.rs
+++ src/arithmetic/subtraction.rs
@@ -3,12 +3,29 @@ use crate::{
     bits::{IfB0, Lazy, Thunk},
 };
 
+/// Returns the difference between the two given bitstrings. See [`Subtract`] for how this handles
+/// underflows.
 pub type Diff<A /*: Bytes*/, B /*: Bytes*/> = <A as Subtract>::Difference<B>;
 
+/// A trait for the subtraction of two bitstrings. This is implemented for all bitstrings for
+/// convenience, but will provide sane results only for the subtraction of a small bitstring from a
+/// large bitstring, i.e. where the output is positive.
+///
+/// When used in a way that would produce a *negative* answer, this will return the
+/// "locally-modular" wrapping subtraction. In other words, if you subtract `110 - 1011` (6 - 11),
+/// this will produce `1011` (11), because `6 - 11 = 11 (mod 16)`. This is the definition of
+/// wrapping subtraction, with the modulus being `2^n` for a bitstring of length `n`. When your
+/// bitstrings are of two different lengths, `n` *should* be the length of the larger of the two,
+/// though due to internal trimming behaviour, this might not always work. In short, use caution
+/// when doing subtraction that would give a negative real number!
 pub trait Subtract: Bitstring {
+    /// The difference of this bitstring with the given one.
     type Difference<Rhs: Bitstring>: Bitstring;
 
-    type DifferenceWithBorrow<Rhs: Bitstring, CarryIn: Bit>: Bitstring;
+    /// An internal associated type that performs the difference of this bitstring with the given
+    /// one, with an additional parameter for the input borrow bit. Generally, end users won't
+    /// need to use this unless building their own arithmetic routines.
+    type DifferenceWithBorrow<Rhs: Bitstring, Borrow: Bit>: Bitstring;
 }
 impl<B: Bitstring> Subtract for B {
     // Full difference is just the partial difference with an initial borrow of zero
@@ -30,6 +47,8 @@ impl<B: Bitstring> Subtract for B {
     >;
 }
 
+/// An internal recursion type for subtracting two bitstrings. You shouldn't need to interact with
+/// this as an end user.
 pub struct SubtractRecurse<A: Bitstring, B: Bitstring, BorrowIn: Bit> {
     _phantom: ::std::marker::PhantomData<(A, B, BorrowIn)>,
 }
@@ -40,8 +59,12 @@ impl<A: Bitstring, B: Bitstring, BorrowIn: Bit> Lazy for SubtractRecurse<A, B, B
     >;
 }
 
+/// A half-subtractor type-level circuit for individual bits.
 pub trait HalfSubtract: Bit {
+    /// The difference of this bit with the given one, done under the given borrow.
     type Difference<Rhs: Bit, BorrowIn: Bit>: Bit;
+    /// The borrow-out bit produced from subtracting this bit and the given one, in the context of
+    /// the given borrow-in bit.
     type Borrow<Rhs: Bit, BorrowIn: Bit>: Bit;
 }
 impl<B: Bit> HalfSubtract for B {
