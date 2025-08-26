54741e09187664ea84737a9a56dc0a4b81d1a328
arctic-hen7 <arctic.hen@pm.me>
2025-08-27 09:56:20 +1000

    feat: added array slice interpretation mechanics
    Actual runtime code!!

--- src/array.rs
+++ src/array.rs
@@ -1,4 +1,5 @@
 use crate::{Bitstring, bits::IsB0, conditional_system};
+use std::mem::{ManuallyDrop, MaybeUninit};
 
 /// A stack-allocated array storing instances of `T`, whose length is defined by the [`Bitstring`]
 /// `B`. This is almost identical to `generic_array`, except it avoids any `where` clauses, and
@@ -52,6 +53,185 @@ use crate::{Bitstring, bits::IsB0, conditional_system};
 #[repr(transparent)]
 pub struct Array<T, N: Bitstring> {
     data: <N as HasArray>::ArrayType<T>,
+}
+impl<T, N: Bitstring> Array<T, N> {
+    /// Creates a new [`Array<T, N>`] of uninitialised elements.
+    pub const fn uninit() -> Array<MaybeUninit<T>, N> {
+        // SAFETY: An uninitialized `[MaybeUninit<_>; N]` is valid, same as a regular array.
+        unsafe { MaybeUninit::<Array<MaybeUninit<T>, N>>::uninit().assume_init() }
+    }
+
+    /// Gets the contents of this [`Array<T, N>`] as a slice. Because we have the same underlying
+    /// memory representation as a slice, this works. The returned slice is guaranteed to have
+    /// length [`Self::len()`] (equivalently [`N::UNSIGNED`]).
+    pub const fn as_slice(&self) -> &[T] {
+        let slice_size = N::UNSIGNED;
+        // Because of the transparent representation, we can ignore all the zero-sized filler stuff
+        // and just get a direct pointer to a bunch of `T`s
+        let ptr = self as *const Self as *const T;
+
+        // SAFETY: We have something in memory that is exactly equivalent to a `[u8; N::UNSIGNED]`.
+        unsafe { std::slice::from_raw_parts(ptr, slice_size) }
+    }
+
+    /// Gets the contents of this [`Array<T, N>`] as a mutable slice.
+    pub const fn as_mut_slice(&mut self) -> &mut [T] {
+        let slice_size = N::UNSIGNED;
+        // Because of the transparent representation, we can ignore all the zero-sized filler stuff
+        // and just get a direct pointer to a bunch of `T`s
+        let ptr = self as *mut Self as *mut T;
+
+        // SAFETY: We have something in memory that is exactly equivalent to a `[u8; N::UNSIGNED]`.
+        unsafe { std::slice::from_raw_parts_mut(ptr, slice_size) }
+    }
+
+    /// Tries to construct an [`Array<T, N>`] from the given slice. This returns a reference, as
+    /// we're just reinterpreting the given slice as our own type. This will fail if the given
+    /// slice has the wrong length.
+    pub const fn try_from_slice(slice: &[T]) -> Result<&Self, BadLength> {
+        if slice.len() != N::UNSIGNED {
+            return Err(BadLength {
+                found: slice.len(),
+                expected: N::UNSIGNED,
+            });
+        }
+
+        // SAFETY: Again, `Array<T, N>` has the same in-memory representation as a slice, so we can
+        // literally just "rename" the type of this slice.
+        Ok(unsafe { &*(slice.as_ptr() as *const Self) })
+    }
+
+    /// Constructs an [`Array<T, N>`] from the given slice.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the given slice is the wrong length.
+    pub const fn from_slice(slice: &[T]) -> &Self {
+        if slice.len() != N::UNSIGNED {
+            panic!("tried to construct array from slice of incorrect length");
+        }
+
+        match Self::try_from_slice(slice) {
+            Ok(s) => s,
+            Err(_) => unreachable!(),
+        }
+    }
+
+    /// Tries to construct an [`Array<T, N>`] from the given mutable slice. This returns a mutable
+    /// reference, as we're just reinterpreting the given mutable slice as our own type. This will
+    /// fail if the given slice has the wrong length.
+    pub const fn try_from_mut_slice(slice: &mut [T]) -> Result<&mut Self, BadLength> {
+        if slice.len() != N::UNSIGNED {
+            return Err(BadLength {
+                found: slice.len(),
+                expected: N::UNSIGNED,
+            });
+        }
+
+        // SAFETY: Again, `Array<T, N>` has the same in-memory representation as a slice, so we can
+        // literally just "rename" the type of this slice
+        Ok(unsafe { &mut *(slice.as_mut_ptr() as *mut Self) })
+    }
+
+    /// Constructs an [`Array<T, N>`] from the given mutable slice.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the given slice is the wrong length.
+    pub const fn from_mut_slice(slice: &mut [T]) -> &mut Self {
+        if slice.len() != N::UNSIGNED {
+            panic!("tried to construct array from slice of incorrect length");
+        }
+
+        match Self::try_from_mut_slice(slice) {
+            Ok(s) => s,
+            Err(_) => unreachable!(),
+        }
+    }
+
+    /// Returns the length of thise [`Array<T, N>`], which is equal to [`N::UNSIGNED`].
+    pub const fn len() -> usize {
+        N::UNSIGNED
+    }
+}
+impl<T, N: Bitstring> Array<MaybeUninit<T>, N> {
+    /// Assumes this array of [`MaybeUninit<T>`] has all elements initialized.
+    ///
+    /// # Safety
+    ///
+    /// All elements must actually be initialized, or this will lead to undefined behaviour due to
+    /// the underlying transmutation.
+    pub const unsafe fn assume_init(self) -> Array<T, N> {
+        // SAFETY: There's no difference between `MaybeUninit<T>` and `T` in memory (literally a
+        // union with `()`), so perfectly safe to reinterpret the array as a whole
+        unsafe { const_transmute::<_, _>(self) }
+    }
+}
+impl<T: Default, N: Bitstring> Default for Array<T, N> {
+    fn default() -> Self {
+        let mut uninit = Self::uninit();
+        for elem in uninit.as_mut_slice() {
+            elem.write(T::default());
+        }
+
+        // SAFETY: There's no difference between `MaybeUninit<T>` and `T` in memory (literally a
+        // union with `()`), so perfectly safe to reinterpret the array as a whole
+        unsafe { const_transmute::<_, Self>(uninit) }
+    }
+}
+impl<T, N: Bitstring> AsRef<[T]> for Array<T, N> {
+    fn as_ref(&self) -> &[T] {
+        self.as_slice()
+    }
+}
+impl<T, N: Bitstring> AsMut<[T]> for Array<T, N> {
+    fn as_mut(&mut self) -> &mut [T] {
+        self.as_mut_slice()
+    }
+}
+
+/// Transmutes from `A` to `B`, but at const evaluation time. This is equivalent to
+/// [`std::mem::transmute`] in all other respects, and the same safety contracts must be upheld.
+///
+/// # Safety
+///
+/// See [`std::mem::transmute`], but in general, the bits you provide must be valid in both `A` and
+/// `B`.
+const unsafe fn const_transmute<A, B>(a: A) -> B {
+    if std::mem::size_of::<A>() != std::mem::size_of::<B>() {
+        panic!("size mismatch in const transmute");
+    }
+
+    // This will hold either `A` or `B`, and because their sizes are equal, will have whatever that
+    // size is
+    #[repr(C)]
+    union Union<A, B> {
+        a: ManuallyDrop<A>,
+        b: ManuallyDrop<B>,
+    }
+
+    // We instantiate a manually-droppable version of `a` to make sure we don't drop the data when
+    // we transmute
+    let a = ManuallyDrop::new(a);
+    // Then we put that `a` into our magical union
+    let union = Union::<A, B> { a };
+    // Then we extract it from said union, but as if it were `b`
+    //
+    // SAFETY: `a` and `b` have the same size (guaranteed above), and the caller has guaranteed to
+    // us that the bits are valid in `B`
+    let b_transmute = unsafe { union.b };
+    // And finally we just extract from the `ManuallyDrop` so we have a normal, droppable type
+    // again
+    ManuallyDrop::into_inner(b_transmute)
+}
+
+/// The error that occurs when we try to convert from a slice into an [`Array<T, N>`], but the
+/// length is wrong.
+#[derive(Error, Debug)]
+#[error("bad slice length: expected {expected}, found {found}")]
+pub struct BadLength {
+    found: usize,
+    expected: usize,
 }
 
 /// An internal struct that represents the even side of an array. If we have an array of length 6,
@@ -135,6 +315,7 @@ impl<T, B: Bitstring> Lazy for ArrayOddRecurse<T, B> {
 }
 
 use array_conditionals::{If, Lazy, Thunk};
+use thiserror::Error;
 conditional_system!(pub array_conditionals, super::sealed::IsArrayImpl);
 
 #[test]
@@ -152,4 +333,17 @@ fn arrays() {
     assert_eq!(size_of::<A3>(), 3);
     assert_eq!(size_of::<A14>(), 14);
     assert_eq!(size_of::<A5Long>(), 4 * 5);
+}
+
+#[test]
+fn arrays_runtime() {
+    use crate::bs;
+
+    type A5Long = Array<u32, bs!(1, 0, 1)>;
+
+    assert!(A5Long::try_from_slice(&[0u32; 5]).is_ok());
+    assert!(A5Long::try_from_slice(&[0u32; 4]).is_err());
+
+    let zeroed = A5Long::default();
+    assert_eq!(zeroed.as_slice(), &[0u32; 5]);
 }
